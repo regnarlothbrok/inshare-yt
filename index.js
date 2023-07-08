bb51cc415b8a0a83058e504ef4883d208987f02a
regnarlothbrok <106487095+regnarlothbrok@users.noreply.github.com>
2023-07-09 02:47:48 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,57 +1,55 @@
 const dropZone = document.querySelector(".drop-zone")
 const fileinput = document.querySelector("#fileinput");
 const browsebtn = document.querySelector(".browsebtn")
-const bgProgress = document.querySelector("bg.progress");
+const bgProgress = document.querySelector(".bg-progress");
 const progressBar = document.querySelector(".progress-bar")
 const percentDiv = document.querySelector("#percent")
 const progressContainer = document.querySelector(".progress-container")
 const sharingContainer = document.querySelector('.sharing-container')
 const fileURLInput = document.querySelector('#fileURL')
-const copBtnL = document.querySelector('#copyBTN')
+const copyBtn = document.querySelector('#copyBtn')
 const emailForm = document.querySelector('#emailForm')
 const toast = document.querySelector('.toast')
 
 const host = "https://coruscating-elf-04359a.netlify.app/"
 const uploadURL = `${host}api/files`;
-const emailURL = `${host}api/files/send`; 
+const emailURL = `${host}api/files/send`;
 // const uploadURL = `${host}api/files`;
-const maxAllowedSize = 100 * 1024 *1024;
+const maxAllowedSize = 100 * 1024 * 1024;
 
-dropZone.addEventListener("dragover", (e) =>{
+dropZone.addEventListener("dragover", (e) => {
     console.log("dragging");
     e.preventDefault();
-    if(!dropZone.classList.contains("dragged")) {
+    if (!dropZone.classList.contains("dragged")) {
         dropZone.classList.add("dragged")
     }
-    
+
 })
 
-dropZone.addEventListener("dragleave",() =>{
+dropZone.addEventListener("dragleave", () => {
     dropZone.classList.remove("dragged")
 })
 
-fileinput.addEventListener("change", ()=>{
+fileinput.addEventListener("change", () => {
     uploadFile();
 })
 
-dropZone.addEventListener("drop", (e) =>{
+dropZone.addEventListener("drop", (e) => {
     e.preventDefault();
     dropZone.classList.remove("dragged");
     const files = e.dataTransfer.files;
     // console.log(files);
-    if(files.length) {
-        fileinput.files=files;
+    if (files.length) {
+        fileinput.files = files;
     }
     uploadFile();
 
 
- })
+})
 
-    fileinput.addEventListener("change", ()=> {
-        uploadFile();
-    })
 
-    browsebtn.addEventListener("click", ()=>{
+
+browsebtn.addEventListener("click", () => {
     fileinput.click()
 })
 
@@ -62,97 +60,97 @@ copyBtn.addEventListener("click", () => {
     document.execCommand("copy")
     showToast("Link Copied");
 })
-const uploadFile =()=> {
+const uploadFile = () => {
 
     const file = fileinput.files[0];
-    if(fileinput.files>1) {
-       resetFileInput()
+    if (fileinput.files > 1) {
+        resetFileInput()
         showToast("Upload only 1 file!")
         return;
     }
-    if(file.size >maxAllowedSize ) {
+    if (file.size > maxAllowedSize) {
         resetFileInput()
         showToast("Can't upload more than 100MB")
         return;
     }
     progressContainer.style.display = "block";
     const formData = new FormData();
-    formData.append("myfile",file);
+    formData.append("myfile", file);
 
     const xhr = new XMLHttpRequest();
     //
-    xhr.onreadystatechange =()=> {
-      if(xhr.readyState === XMLHttpRequest.DONE) {
-        console.log(xhr.response);
-        onFileUploadSuccess(JSON.parse(xhr.responseText));
-      }
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            console.log(xhr.response);
+            onFileUploadSuccess(JSON.parse(xhr.responseText));
+        }
     }
 
-    xhr.upload.ongrograss = updateProgress;
-    xhr.upload.onerror =()=>{
+    xhr.upload.onpgrograss = updateProgress;
+    xhr.upload.onerror = () => {
         resetFileInput()
         showToast(`Error in upload: ${xhr.statusText}`)
     }
 
-    xhr.open("POST",uploadURL);
+    xhr.open("POST", uploadURL);
     xhr.send(formData)
 };
 
 const updateProgress = (e) => {
-     const percent = Math.round((e.loaded / e.total)*100);
+    const percent = Math.round((e.loaded / e.total) * 100);
     //  console.log(percent);
-     bgProgress.style.width = `${percent}%`
-     percentDiv.innerText = percent;
-     progressBar.style.transform = `scaleX(${percent/100}/)`
+    bgProgress.style.width = `${percent}%`
+    percentDiv.innerText = percent;
+    progressBar.style.transform = `scaleX(${percent / 100}/)`
 
 }
 
-const onFileUploadSuccess = ({file: url}) => {
-      console.log(e)
-      resetFileInput()
-      emailForm[2].removeAttribute("disabled")
+const onFileUploadSuccess = ({ file: url }) => {
+    console.log(e)
+    resetFileInput()
+    emailForm[2].removeAttribute("disabled")
 
-      progressContainer.style.display = "none"
-      sharingContainer.style.display = "block"
-      fileURLInput.value = url;
+    progressContainer.style.display = "none"
+    sharingContainer.style.display = "block"
+    fileURLInput.value = url;
 
 }
 
- const resetFileInput = () =>{
+const resetFileInput = () => {
     fileinput.value = "";
- }
+}
 
-emailForm.addEventListener("submit",(e) => {
+emailForm.addEventListener("submit", (e) => {
     e.preventDefault()
     const url = fileURLInput.value;
     const formData = {
-        uuid: url.split("/").splice(-1,1)[0],
-        emailTo: emailForm.elements["to-email"],
+        uuid: url.split("/").splice(-1, 1)[0],
+        emailTo: emailForm.elements["to-email"].value,
         emailFrom: emailForm.elements["from-email"].value,
     }
 
-    emailForm[2].setAttribute("disabled","true")
+    emailForm[2].setAttribute("disabled", "true")
     console.table(formData)
 
     fetch(emailURL, {
         method: "POST",
         headers: {
-            "Content-Type":"application/json"
+            "Content-Type": "application/json"
         },
         body: JSON.stringify(formData)
     }).then((res) => res.json())
-      .then(({success}) =>{
-          sharingContainer.style.display = "none";
-          showToast("Email sent")
-    });
+        .then(({ success }) => {
+            sharingContainer.style.display = "none";
+            showToast("Email sent")
+        });
 })
 
 let toastTimer;
-const showToast=(msg) => {
+const showToast = (msg) => {
     toast.innerText = msg;
     toast.style.transform = "translate(-50%,0)"
     clearTimeout(toastTimer);
-    toastTimer = setTimeout(()=>{
-    toast.style.transform = "translate(-50%,60px)" 
-    },2000);
+    toastTimer = setTimeout(() => {
+        toast.style.transform = "translate(-50%,60px)"
+    }, 2000);
 }
